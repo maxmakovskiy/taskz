eb23eb2cdbd2966db15df19a0d583efb18e1266a
maxmakovskiy <makovskiymax@gmail.com>
2021-10-24 18:46:44 +0300

    + - == added for rational number

---
+++ rational_numbers_sub_add/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <numeric>
+using namespace std;
+
+class Rational {
+public:
+    Rational() {
+        numerator = 0;
+        denominator = 1;
+    }
+
+    Rational(int numerator, int denominator) {
+        if (numerator < 0 && denominator < 0)
+        {
+            numerator = -1 * numerator;
+            denominator = -1 * denominator;
+        }
+        else if (denominator < 0)
+        {
+            numerator = -1 * numerator;
+            denominator = -1 * denominator;
+        }
+
+        int nod = gcd(numerator, denominator);
+        if (nod == 1)
+        {
+            this->numerator = numerator;
+            this->denominator = denominator;
+        }
+        else
+        {
+            this->numerator = numerator / nod;
+            this->denominator = denominator / nod;
+        }
+    }
+
+    int Numerator() const {
+        return numerator; 
+    }
+
+    int Denominator() const {
+        return denominator;
+    }
+
+private:
+    int numerator;
+    int denominator;
+
+};
+
+Rational operator+(const Rational& lhs, const Rational& rhs)
+{
+    return Rational(lhs.Numerator() * rhs.Denominator() + rhs.Numerator() * lhs.Denominator(),
+            lhs.Denominator() * rhs.Denominator());
+}
+
+Rational operator-(const Rational& lhs, const Rational& rhs)
+{
+    return Rational(lhs.Numerator() * rhs.Denominator() - rhs.Numerator() * lhs.Denominator(),
+            lhs.Denominator() * rhs.Denominator());
+}
+
+
+bool operator==(const Rational& lhs, const Rational& rhs)
+{
+    return (lhs.Numerator() == rhs.Numerator()) && (lhs.Denominator() == rhs.Denominator());
+}
+
+
+
+int main() {
+    {
+        Rational r1(4, 6);
+        Rational r2(2, 3);
+        bool equal = r1 == r2;
+        if (!equal) {
+            cout << "4/6 != 2/3" << endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a + b;
+        bool equal = c == Rational(2, 1);
+        if (!equal) {
+            cout << "2/3 + 4/3 != 2" << endl;
+            return 2;
+        }
+    }
+
+    {
+        Rational a(5, 7);
+        Rational b(2, 9);
+        Rational c = a - b;
+        bool equal = c == Rational(31, 63);
+        if (!equal) {
+            cout << "5/7 - 2/9 != 31/63" << endl;
+            return 3;
+        }
+    }
+
+    cout << "OK" << endl;
+    return 0;
+}
