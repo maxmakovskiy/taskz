93c0a5d46aab0934e47cf06b90ebc0a60b4b5bbc
maxmakovskiy <makovskiymax@gmail.com>
2021-10-24 19:03:42 +0300

    mult div added to rational number

--- rational_numbers_sub_add/main.cpp
+++ rational_numbers_sub_add/main.cpp
@@ -66,7 +66,17 @@ bool operator==(const Rational& lhs, const Rational& rhs)
     return (lhs.Numerator() == rhs.Numerator()) && (lhs.Denominator() == rhs.Denominator());
 }
 
+Rational operator*(const Rational& lhs, const Rational& rhs)
+{
+    return Rational(lhs.Numerator() * rhs.Numerator(),
+            lhs.Denominator() * rhs.Denominator());
+}
 
+Rational operator/(const Rational& lhs, const Rational& rhs)
+{
+    return Rational(lhs.Numerator() * rhs.Denominator(),
+            lhs.Denominator() * rhs.Numerator());
+}
 
 int main() {
     {
@@ -98,6 +108,28 @@ int main() {
         if (!equal) {
             cout << "5/7 - 2/9 != 31/63" << endl;
             return 3;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a * b;
+        bool equal = c == Rational(8, 9);
+        if (!equal) {
+            cout << "2/3 * 4/3 != 8/9" << endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(5, 4);
+        Rational b(15, 8);
+        Rational c = a / b;
+        bool equal = c == Rational(2, 3);
+        if (!equal) {
+            cout << "5/4 / 15/8 != 2/3" << endl;
+            return 2;
         }
     }
 
