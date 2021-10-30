04a1c9c46cd41db63b8e58ce7351964ad302eb4e
maxmakovskiy <makovskiymax@gmail.com>
2021-10-30 15:07:37 +0300

    Yellow 4-2 merge sort 1

---
+++ merge_sort1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end)
+{
+    if (std::distance(range_begin, range_end) < 2)
+    {
+        return;
+    }
+
+    std::vector<typename RandomIt::value_type> elements(range_begin, range_end);
+
+    auto middle = elements.begin() + elements.size() / 2;
+    MergeSort(elements.begin(), middle);
+    MergeSort(middle, elements.end());
+
+    std::merge(elements.begin(), middle, middle, elements.end(), range_begin);
+
+}
+
+int main()
+{
+    std::vector<int> v = {6, 4, 7, 6, 4, 4, 0, 1};
+
+    MergeSort(std::begin(v), std::end(v));
+
+    for (int x : v) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
