7560e93cbe5582e335d5cd9164867045a532ad45
maxmakovskiy <makovskiymax@gmail.com>
2021-11-07 14:10:09 +0200

    Minor refactoring

--- simple_unit_testing/test.h
+++ simple_unit_testing/test.h
@@ -117,18 +117,18 @@ private:
     int fail_count = 0;
 };
 
-#define ASSERT_EQUAL(x, y) {            \
-    std::ostringstream os;              \
-    os << #x << " != " << #y << ", "    \
-       << __FILE__ << ":" << __LINE__;  \
-    AssertEqual(x, y, os.str());        \
+#define ASSERT_EQUAL(x, y) {                              \
+    std::ostringstream osAssertEqualTesting;              \
+    osAssertEqualTesting << #x << " != " << #y << ", "    \
+       << __FILE__ << ":" << __LINE__;                    \
+    AssertEqual(x, y, osAssertEqualTesting.str());        \
 }
 
-#define ASSERT(x) {                     \
-    std::ostringstream os;              \
-    os << #x << " is false, "           \
-       << __FILE__ << ":" << __LINE__;  \
-    Assert(x, os.str());                \
+#define ASSERT(x) {                                \
+    std::ostringstream osUnitTesting;              \
+    osUnitTesting << #x << " is false, "           \
+       << __FILE__ << ":" << __LINE__;             \
+    Assert(x, osUnitTesting.str());                \
 }
 
 #define RUN_TEST(tr, func) tr.RunTest(func, #func)
