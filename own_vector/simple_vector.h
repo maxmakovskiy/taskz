771f98fb8375fc939942fba4784fe1e0f1364447
maxmakovskiy <makovskiymax@gmail.com>
2021-11-11 08:00:46 +0200

    Red 3 vector through pointers

---
+++ own_vector/simple_vector.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <cstdlib>
+#include <algorithm>
+
+// Реализуйте шаблон SimpleVector
+template <typename T>
+class SimpleVector 
+{
+public:
+    SimpleVector() {}
+    explicit SimpleVector(size_t size)
+    {
+        this->size = size;
+        this->capacity = size * 2;
+        data = new T[capacity];
+    }
+
+    ~SimpleVector()
+    {
+        delete[] data;
+    }
+
+    T& operator[](size_t index) { return *(data + index); }
+
+    T* begin() { return data; }
+
+    T* end() { return (data + size); }
+
+    size_t Size() const { return size; }
+
+    size_t Capacity() const { return capacity; }
+
+    void PushBack(const T& value)
+    {
+        if (!data) {
+            capacity = 1u;
+            data = new T[capacity];
+        } else if (size == capacity) {
+            capacity *= 2;
+            T* newLocation = new T[capacity];
+            std::copy(begin(), end(), newLocation);
+            delete[] data;
+            data = newLocation;
+        }
+
+        *(data + size) = value;
+        size++;
+    }
+
+private:
+    size_t size = 0u;
+    size_t capacity = 0u;
+    T* data = nullptr;
+};
