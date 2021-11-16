24a0753c402cc2ded32dc4814487482635a65b64
maxmakovskiy <makovskiymax@gmail.com>
2021-11-16 10:04:03 +0200

    save mutex for each bucket -> optimized to 100 locks 66ms

--- concurrent_map/map.h
+++ concurrent_map/map.h
@@ -7,6 +7,10 @@ T abs(T val)
     return val < 0 ? -val : val;
 }
 
+auto lock(std::mutex& m)
+{
+    return std::lock_guard<std::mutex>(m);
+}
 
 template <typename K, typename V>
 class ConcurrentMap 
@@ -19,6 +23,11 @@ public:
     {
         std::lock_guard<std::mutex> guard;
         V& ref_to_value;
+
+        Access(const K& key, std::pair<std::mutex, std::map<K, V>>& bucket)
+            : guard(bucket.first)
+            , ref_to_value(bucket.second[key])
+        {}
     };
 
     explicit ConcurrentMap(size_t bucket_count)
@@ -33,24 +42,23 @@ public:
         // if element with given key doesn't exist in any bucket
         // then we write them to first backet
         size_t bucket_idx = abs(key) % base.size();
+        std::pair<std::mutex, std::map<K,V>>& bucket = base[bucket_idx];
         
-        return { std::lock_guard<std::mutex>(m), base[bucket_idx][key]};
+        return { key, bucket }; 
     }
 
     std::map<K, V> BuildOrdinaryMap()
     {
-        std::lock_guard<std::mutex> g(m);
         std::map<K, V> result;
-        for (const auto& m : base) {
-            result.insert(m.begin(), m.end()); 
+        for (auto& [_mutex, _map] : base) {
+            auto guard = lock(_mutex);
+            result.insert(_map.begin(), _map.end()); 
         }
         return result;
     }
 
-
-
 private:
-    std::vector<std::map<K, V>> base;
-    std::mutex m;
+    // save for each bucket own mutex
+    std::vector<std::pair<std::mutex, std::map<K, V>>> base;
 };
 
